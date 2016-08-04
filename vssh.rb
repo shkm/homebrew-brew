b17ac3663ee1029867eefe8ce0fd0840d86a2dc9
Jamie Schembri <jamie@lico.nl>
2016-08-04 07:21:54 +0200

    vssh 2.1.2

--- vssh.rb
+++ vssh.rb
@@ -1,8 +1,8 @@
 class Vssh < Formula
   desc "Fast `vagrant ssh` in the current directory."
   homepage "https://github.com/shkm/vssh"
-  url "https://github.com/shkm/vssh/archive/2.1.1.tar.gz"
-  sha256 "fbe225dd8b49daaecee41aafa1677ba5dc66c71961709227a0bab2247fbdb411"
+  url "https://github.com/shkm/vssh/archive/2.1.2.tar.gz"
+  sha256 "7e5e46dde0f7725898af634ed237efa6310d418becda654c5b158bb032bdd36f"
 
   def install
     bin.install "vssh"
